6fa060f73ad7731b2b70f3512d77e004f59da521
Anastasiia Hrechanyk <77104631+anastasiiahrechanyk@users.noreply.github.com>
2025-10-29 17:37:03 +0100

    tree_4_kth_smallest_in_bst.py

---
+++ tree_4_kth_smallest_in_bst.py
@@ -0,0 +1,42 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def kthSmallest(root: TreeNode, k: int) -> int:
+    stack = []
+    while True:
+        while root:
+            stack.append(root)
+            root = root.left
+        root = stack.pop()
+        k -= 1
+        if k == 0:
+            return root.val
+        root = root.right
+
+def build_tree(values):
+    if not values:
+        return None
+    nodes = [TreeNode(v) if v is not None else None for v in values]
+    kids = nodes[::-1]
+    root = kids.pop()
+    for node in nodes:
+        if node:
+            if kids: node.left = kids.pop()
+            if kids: node.right = kids.pop()
+    return root
+
+if __name__ == "__main__":
+    root = build_tree([3, 1, 4, None, 2])
+    print("Приклад 1:", kthSmallest(root, 1))  
+
+    root = build_tree([5, 3, 6, 2, 4, None, None, 1])
+    print("Приклад 2:", kthSmallest(root, 3))  
+
+
+
+
+
+
